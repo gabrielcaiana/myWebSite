8223a2e7f55aa028e573e1063510776ab6ca8da8
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-09-23 01:14:09 -0300

    create home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <h1>Home</h1>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div class=" container flex flex-col items-center">
+      <h1>Heey, I`m Gabriel Caiana!</h1>
+      <p>
+        I'm a 24-year-old software developer working primarily with JavaScript |
+        TypeScript | Vue and Nuxt. Passionate about technology, innovation and
+        UX | User interface design. I'm currently studying technologist in
+        systems analysis and development and specializing in software
+        development with a focus on usability and accessibility, in addition to
+        providing a good experience for users.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
