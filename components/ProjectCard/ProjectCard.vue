3127a9386c06978d039e37610b73a08de33284b3
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-10-21 22:12:10 -0300

    test: ProjectCards

--- components/ProjectCard/ProjectCard.vue
+++ components/ProjectCard/ProjectCard.vue
@@ -1,57 +1,113 @@
 <template>
-  <article
-    class="
-      relative
-      w-full
-      h-64
-      bg-cover bg-center
-      group
-      rounded-lg
-      overflow-hidden
-      shadow-lg
-      hover:shadow-2xl
-      transition
-      duration-300
-      ease-in-out
-    "
-    :style="`background-image: url('${card.cover}');`"
-  >
+  <div class="rounded-lg">
     <div
       class="
-        absolute
-        inset-0
-        bg-black bg-opacity-50
-        group-hover:opacity-75
-        transition
-        duration-300
-        ease-in-out
-      "
-    ></div>
-    <div
-      class="
+        bg-white bg-opacity-5
+        rounded-lg
+        shadow-lg
         relative
-        w-full
-        h-full
-        px-4
-        sm:px-6
-        lg:px-4
+        hover:shadow-2xl
+        transition
+        duration-500
         flex
-        justify-center
-        items-center
+        flex-col
       "
     >
-      <h3 class="text-center">
-        <a
-          class="text-white text-2xl font-bold text-center"
-          :href="card.link"
-          target="_blank"
+      <article
+        class="
+          relative
+          w-auto
+          h-64
+          bg-cover bg-center
+          group
+          overflow-hidden
+          shadow-lg
+          hover:shadow-2xl
+          rounded-t-lg
+          transition
+          duration-300
+          ease-in-out
+        "
+        :style="`background-image: url('${card.cover}');`"
+      >
+        <div
+          class="
+            absolute
+            inset-0
+            bg-black bg-opacity-50
+            group-hover:opacity-75
+            transition
+            duration-300
+            ease-in-out
+          "
+        ></div>
+        <div
+          class="
+            relative
+            w-full
+            h-full
+            px-4
+            sm:px-6
+            lg:px-4
+            flex
+            justify-center
+            items-center
+          "
         >
-          <span class="absolute inset-0"></span>
+          <h3 class="text-center">
+            <a
+              class="text-white text-2xl font-bold text-center"
+              :href="card.link"
+              target="_blank"
+            >
+              <span class="absolute inset-0"></span>
+            </a>
+          </h3>
+        </div>
+      </article>
+      <div class="px-4 pb-4">
+        <h1 class="text-2xl font-semibold">
           {{ card.title }}
-        </a>
-      </h3>
+        </h1>
+        <p class="text-gray-400 leading-6 tracking-normal">
+          {{ card.description }}
+        </p>
+        <button
+          class="py-2 px-4 mt-8 bg-blue-500 text-white rounded-md shadow-xl"
+        >
+          Learn More
+        </button>
+        <div
+          class="
+            flex
+            justify-end
+            absolute
+            top-0
+            right-0
+            transform
+            translate-x-2
+            -translate-y-3
+            w-full
+          "
+        >
+          <span
+            v-for="(technologie, index) in card.technologies"
+            :key="index"
+            class="
+              py-2
+              px-6
+              ml-2
+              text-sm text-white
+              rounded-md
+              shadow-xl
+            "
+            :style="`background: ${technologie.color}`"
+            >{{ technologie.name }}</span
+          >
+        </div>
+      </div>
     </div>
-  </article>
+  </div>
 </template>
 
 <script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,14 +1,14 @@
 <template>
   <Container>
     <h1>Projects</h1>
-    <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mb-12">
+    <section class="max-w-7xl mx-auto mb-12">
       <article>
         <section
           class="
             mt-6
             grid grid-cols-1
             md:grid-cols-1
-            lg:grid-cols-3
+            lg:grid-cols-2
             gap-x-6 gap-y-8
           "
         >
