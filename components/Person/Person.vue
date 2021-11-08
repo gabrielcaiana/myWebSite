5116e0bfd4d589bb70897fe422c47514f1036d4e
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-11-08 01:13:16 -0300

    update home

---
+++ components/Person/Person.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-56 rounded-full p-2 border-2 border-green-500 cursor-pointer">
+    <a target="_blank" href="https://www.linkedin.com/in/gabrielcaiana/">
+      <img class="rounded-full" src="/images/me.jpeg" alt="Gabriel Caiana">
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <Container class="flex items-center max-w-screen-lg">
     <main class="flex flex-col h-full justify-center items-center">
       <div class="container flex flex-col items-center text-center">
+        <Person />
         <h1>Heey, I`m Gabriel Caiana!</h1>
         <p>
           I'm a 24-year-old software developer working primarily with JavaScript
