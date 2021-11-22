5783be0da9e398af4e66ad9f605abebb2bc33f5e
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-11-22 07:19:09 -0300

    refactor: remove plugin getSiteMeta

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -27,7 +27,6 @@
 
 <script>
 import { formatDate } from '@/utils/date'
-import getSiteMeta from '@/utils/getSiteMeta'
 
 export default {
   async asyncData({ $content, params }) {
@@ -45,28 +44,7 @@ export default {
   head() {
     return {
       title: this.article.title,
-      meta: [
-        ...this.meta,
-        {
-          property: 'article:published_time',
-          content: this.article.createdAt,
-        },
-        {
-          property: 'article:modified_time',
-          content: this.article.updatedAt,
-        },
-        {
-          property: 'article:tag',
-          content: this.article.tags ? this.article.tags.toString() : '',
-        },
-        { name: 'twitter:label1', content: 'Written by' },
-        { name: 'twitter:data1', content: 'Gabriel Caiana' },
-        { name: 'twitter:label2', content: 'Filed under' },
-        {
-          name: 'twitter:data2',
-          content: this.article.tags ? this.article.tags.toString() : '',
-        },
-      ],
+
       link: [
         {
           hid: 'canonical',
@@ -76,20 +54,6 @@ export default {
       ],
     }
   },
-
-  computed: {
-  meta() {
-    const metaData = {
-      type: "article",
-      title: this.article.title,
-      description: this.article.description,
-      url: `${this.$config.axios.browserBaseURL}/articles/${this.$route.params.slug}`,
-      mainImage: this.article.img,
-    };
-    return getSiteMeta(metaData);
-  }
-},
-
   methods: {
     formatDate,
   },
