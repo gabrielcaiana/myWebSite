e6fe792d698ed880e480213409e1815e8a5e2517
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-01-22 17:04:05 -0300

    chore: create env to execute ngrok

--- modules/ngrok/index.js
+++ modules/ngrok/index.js
@@ -4,27 +4,30 @@ import chalk from 'chalk'
 
 export default function () {
   const { nuxt } = this
-  if (!nuxt.options.dev) return
 
-  let url;
+  if (!nuxt.options.env || process.env.NGROK_ACTIVE === "false" ) return
+ 
+  let url
 
   const options = nuxt.options.ngrok || {}
+
   const token = process.env.NGROK_TOKEN || options.token
 
-  nuxt.hook('listen', async ( _server, { port } ) => {
-    
-    if(token) {
+  nuxt.hook('listen', async (_server, { port }) => {
+    if (token) {
       await ngrok.authtoken(token)
     }
-    
+
     url = await ngrok.connect(port)
 
     nuxt.options.publicRuntimeConfig.ngrok = { url }
 
-    nuxt.options.cli.badgeMessages.push(`Public URL: ${chalk.underline.magenta(url)}`)
+    nuxt.options.cli.badgeMessages.push(
+      `Public URL: ${chalk.underline.magenta(url)}`
+    )
   })
 
-  nuxt.hook('close', _ => {
+  nuxt.hook('close', (_) => {
     url && ngrok.disconnect()
   })
 }
