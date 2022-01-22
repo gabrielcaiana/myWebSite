889d04f69f20c16ab695526bb8c34b2e8a9f2386
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-01-22 14:46:18 -0300

    chore: update large text

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <Container class="flex items-center max-w-screen-lg">
+  <Container class="flex items-center justify-center max-w-screen-lg">
     <main class="flex flex-col h-full justify-center items-center">
       <div class="container flex flex-col items-center text-center">
         <Person />
         <h1>Olá, meu nome é Gabriel!</h1>
 
-        <p>
+        <p class="max-w-lg">
           Sou um desenvolvedor de software de 25 anos, trabalho com JavaScript |
           TypeScript | Node | Vue e Nuxt. Apaixonado por tecnologia, inovação e
           UX | UI design.
