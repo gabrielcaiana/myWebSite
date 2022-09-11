0c2ac5bb92c61ccc63e66be7c552840d404d1871
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-09-11 16:38:16 -0300

    chore: update test

--- components/HamburguerMenu/HamburguerMenu.spec.js
+++ components/HamburguerMenu/HamburguerMenu.spec.js
@@ -1,12 +1,14 @@
 import { mount } from '@vue/test-utils'
 import HamburguerMenu from '~/components/HamburguerMenu/HamburguerMenu.vue'
-import MenuMobile from "~/components/MenuMobile/MenuMobile.vue"
+import MenuMobile from '~/components/MenuMobile/MenuMobile.vue'
 
 describe('HamburguerMenu', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(HamburguerMenu, {
-      stubs: { MenuMobile},
+      components: {
+        MenuMobile,
+      },
     })
-    expect(wrapper.vm).toBeTruthy()
+    expect(wrapper.exists()).toBeTruthy()
   })
 })
